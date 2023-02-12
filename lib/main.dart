a6ad5f1e025efb32b893d1bb6b905a604a96387c
James Jilhaney <notail2111@gmail.com>
2023-02-12 12:32:39 +0800

    Added firebase emulators to app

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 import 'app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> main(List<String> args) async {
+  const String host = "10.0.2.2";
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+
+  var firestore = FirebaseFirestore.instance;
+  firestore.settings =
+      const Settings(persistenceEnabled: false, sslEnabled: false);
+  firestore.useFirestoreEmulator(host, 8080);
 
   runApp(const BumbleBee());
 }

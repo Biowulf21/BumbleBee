3fd5db9fca94fd829e83e9f8b7e2426f513fda4e
James Jilhaney <notail2111@gmail.com>
2023-02-13 22:22:20 +0800

    Added create user helper functions

--- lib/repositories/user-repository.dart
+++ lib/repositories/user-repository.dart
@@ -1,9 +1,20 @@
-import 'package:bumblebee/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/user.dart';
+import 'firestore-repository.dart';
 
 class UserRepository {
   // Future<User> getUserInfo() async {
   //   return null;
   // }
 
-  
+  Future<void> createUserFromJSON(
+      {required Map<String, dynamic> dataMap}) async {
+    FirestoreRepository(FirebaseFirestore.instance)
+        .addDocument(collectionID: 'users', dataMap: dataMap);
+  }
+
+  Future<void> createUserFromObject({required User userObject}) async {
+    FirestoreRepository(FirebaseFirestore.instance)
+        .addDocument(collectionID: 'users', dataMap: userObject.toJson());
+  }
 }

39223c0abe0c1628b3ca77e4833f8960ac6d6a59
James Jilhaney <notail2111@gmail.com>
2023-02-14 21:20:01 +0800

    Added unit test for getting all documents

--- test/firebase_repository_test.dart
+++ test/firebase_repository_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:bumblebee/repositories/firestore-repository.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -37,4 +39,24 @@ void main() {
         {'name': 'new test user', 'age': 21, 'cash': 2000.15});
   });
 
+  test('Get all documents from collection', () async {
+    final docs = [];
+    final ids = [];
+    await FirestoreRepository(instance).addDocument(
+        collectionID: 'users',
+        dataMap: {'name': 'user 2', 'age': 99, 'cash': 3.14});
+    final snapshot = await instance.collection('users').get();
+    for (var doc in snapshot.docs) {
+      docs.add(doc.data());
+      ids.add(doc.id);
+    }
+    final dumpShape = {
+      "users": {
+        ids[0]: {"name": "new test user", "age": 21, "cash": 2000.15},
+        ids[1]: {"name": "user 2", "age": 99, "cash": 3.14}
+      }
+    };
+
+    expect(jsonDecode(instance.dump()), dumpShape);
+  });
 }

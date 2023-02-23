1af0b514869e48caf68e36d42cbce5fa7f4aa195
James Jilhaney <notail2111@gmail.com>
2023-02-24 00:58:01 +0800

    Wrote unit test for creating new property as well as use case for creating new property

--- lib/feature/manage properties/domain/usecases/create_property_usecase.dart
+++ lib/feature/manage properties/domain/usecases/create_property_usecase.dart
@@ -1,19 +1,14 @@
 import 'package:bumblebee/core/exceptions/failure.dart';
 import 'package:bumblebee/core/models/property.dart';
 import 'package:bumblebee/core/repositories/firestore_repository.dart';
+import 'package:bumblebee/core/repositories/user-repository.dart';
 import 'package:bumblebee/core/wrappers/firebase_singleton.dart';
+import 'package:bumblebee/feature/authentication/data/models/user.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class ICreatePropertyUseCase {
-  Future<Either<Failure, Property>> createProperty(
-      {required String name,
-      required PropertyType type,
-      Map<String, dynamic>? kwargs});
-}
-
-class CreatePropertyUseCase implements ICreatePropertyUseCase {
-  @override
-  Future<Either<Failure, Property>> createProperty(
+  Future<Either<Failure, String>> createProperty(
       {required name,
       required type,
       hasAdvance,
@@ -24,26 +19,70 @@ class CreatePropertyUseCase implements ICreatePropertyUseCase {
       hasDeposit,
       depositPrice,
       isFullyFurnished,
-      amenities}) async {
-    Property property = Property(
-        name: name,
-        type: type,
-        hasAdvance: hasAdvance,
-        rating: rating,
-        numberOfMonthsAdvance: numberOfMonthsAdvance,
-        id: id,
-        costPerMonthsAdvance: costPerMonthsAdvance,
-        hasDeposit: hasDeposit,
-        depositPrice: depositPrice,
-        isFullyFurnished: isFullyFurnished,
-        amenities: amenities);
+      amenities});
+}
 
+class CreatePropertyUseCase implements ICreatePropertyUseCase {
+  @override
+  Future<Either<Failure, String>> createProperty(
+  {required name,
+  required type,
+  hasAdvance,
+  rating,
+  numberOfMonthsAdvance,
+  id,
+  costPerMonthsAdvance,
+  hasDeposit,
+  depositPrice,
+  isFullyFurnished,
+  amenities,
+}) async {
+  try {
     final String uid = FirebaseSingleton().getAuth.currentUser!.uid;
 
-    final result = await FirestoreRepository(FirebaseSingleton().getFirestore)
-        .addDocument(
-            collectionID: 'users/$uid/properties',
-            successMessage: 'Successfully created new property named $name',
-            dataMap: property.toJson(property));
+    final userDoc = await UserRepository(
+      firestoreInstance: FirebaseSingleton().getFirestore,
+      auth: FirebaseSingleton().getAuth,
+    ).getUserInfo(userID: uid);
+
+    return userDoc.fold(
+      (failure) => Left(Failure(message: failure.message)),
+      (user) async {
+        if (user!.role == userRoles.Tenant) {
+          return const Left(Failure(
+            message: "User is not a landlord. Cannot create a new property with tenant credentials.",
+          ));
+        }
+        Property property = Property(
+          name: name,
+          type: type,
+          hasAdvance: hasAdvance,
+          rating: rating,
+          numberOfMonthsAdvance: numberOfMonthsAdvance,
+          id: id,
+          costPerMonthsAdvance: costPerMonthsAdvance,
+          hasDeposit: hasDeposit,
+          depositPrice: depositPrice,
+          isFullyFurnished: isFullyFurnished,
+          amenities: amenities,
+        );
+
+        final result = await FirestoreRepository(FirebaseSingleton().getFirestore).addDocument(
+          collectionID: 'users/$uid/properties',
+          successMessage: 'Successfully created new property named $name',
+          dataMap: property.toJson(property),
+        );
+
+        return result.fold(
+          (failure) => Left(Failure(message: failure.message)),
+          (SuccessMessage) => Right(SuccessMessage),
+        );
+      },
+    );
+  } on FirebaseException catch (e) {
+    return Left(Failure(message: e.message!));
+  } catch (e) {
+    return Left(Failure(message: e.toString()));
   }
+}
 }
--- test/feature/manage properties/domain/usecases/create_property_usecase_test.dart
+++ test/feature/manage properties/domain/usecases/create_property_usecase_test.dart
@@ -1,11 +1,19 @@
+import 'package:bumblebee/core/models/property.dart';
 import 'package:bumblebee/feature/manage%20properties/domain/usecases/create_property_usecase.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Tests about adding a property', () {
+    const String propertyName = "Test property";
+    const PropertyType type = PropertyType.Single;
     test('Create property writes to a property document on firestore',
         () async {
-      final result = await CreatePropertyUseCase.createProperty();
+      final result = await CreatePropertyUseCase()
+          .createProperty(name: propertyName, type: type);
+      result.fold((failure) => print(failure.message), (successMessage) {
+        expect(successMessage,
+            'Successfully created new property named $propertyName');
+      });
     });
   });
 }

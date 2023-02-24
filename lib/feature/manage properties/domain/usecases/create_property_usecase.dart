3d88997a464dea23734e8449f288cf419a01f25d
James Jilhaney <notail2111@gmail.com>
2023-02-24 12:26:13 +0800

    Added test to fail when user is not logged in, fail when user is not a landlord and restructured code to be more testable

--- lib/feature/manage properties/domain/usecases/create_property_usecase.dart
+++ lib/feature/manage properties/domain/usecases/create_property_usecase.dart
@@ -1,18 +1,19 @@
 import 'package:bumblebee/core/exceptions/failure.dart';
 import 'package:bumblebee/core/models/property.dart';
 import 'package:bumblebee/core/repositories/firestore_repository.dart';
-import 'package:bumblebee/core/repositories/user-repository.dart';
-import 'package:bumblebee/core/wrappers/firebase_singleton.dart';
 import 'package:bumblebee/feature/authentication/data/models/user.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class ICreatePropertyUseCase {
   Future<Either<Failure, String>> createProperty(
       {required String name,
       required PropertyType type,
       required String address,
-      required UserRepository auth,
+      required userRoles userRole,
+      required FirebaseAuth auth,
+      required FirebaseFirestore firestore,
       bool hasAdvance,
       double rating,
       int numberOfMonthsAdvance,
@@ -30,7 +31,9 @@ class CreatePropertyUseCase implements ICreatePropertyUseCase {
       {required String name,
       required PropertyType type,
       required String address,
-      required UserRepository auth,
+      required userRoles userRole,
+      required FirebaseAuth auth,
+      required FirebaseFirestore firestore,
       bool? hasAdvance,
       double? rating,
       int? numberOfMonthsAdvance,
@@ -41,51 +44,53 @@ class CreatePropertyUseCase implements ICreatePropertyUseCase {
       bool? isFullyFurnished,
       List<Amenity>? amenities}) async {
     try {
-      final String uid = FirebaseSingleton().getAuth.currentUser!.uid;
+      if (auth.currentUser == null) {
+        return const Left(Failure(message: 'No current user present.'));
+      }
 
-      final userDoc = await auth.getUserInfo(userID: uid);
+      final String uid = auth.currentUser!.uid;
 
-      return userDoc.fold(
+      if (userRole == userRoles.Tenant) {
+        throw const Failure(
+          message: 'permission_denied',
+        );
+      }
+
+      Property property = Property(
+        name: name,
+        type: type,
+        address: address,
+        hasAdvance: hasAdvance,
+        rating: rating,
+        numberOfMonthsAdvance: numberOfMonthsAdvance,
+        ownerID: auth.currentUser!.uid,
+        costPerMonthsAdvance: costPerMonthsAdvance,
+        hasDeposit: hasDeposit,
+        depositPrice: depositPrice,
+        isFullyFurnished: isFullyFurnished,
+        amenities: amenities,
+      );
+
+      final result = await FirestoreRepository(firestore).addDocument(
+        collectionID: 'users/$uid/properties',
+        successMessage: 'Successfully created new property named $name',
+        dataMap: property.toJson(property),
+      );
+
+      return result.fold(
         (failure) => Left(Failure(message: failure.message)),
-        (user) async {
-          if (user!.role == userRoles.Tenant) {
-            return const Left(Failure(
-              message:
-                  "User is not a landlord. Cannot create a new property with tenant credentials.",
-            ));
-          }
-
-          Property property = Property(
-            name: name,
-            type: type,
-            address: address,
-            hasAdvance: hasAdvance,
-            rating: rating,
-            numberOfMonthsAdvance: numberOfMonthsAdvance,
-            ownerID: FirebaseSingleton().getAuth.currentUser!.uid,
-            costPerMonthsAdvance: costPerMonthsAdvance,
-            hasDeposit: hasDeposit,
-            depositPrice: depositPrice,
-            isFullyFurnished: isFullyFurnished,
-            amenities: amenities,
-          );
-
-          final result =
-              await FirestoreRepository(FirebaseSingleton().getFirestore)
-                  .addDocument(
-            collectionID: 'users/$uid/properties',
-            successMessage: 'Successfully created new property named $name',
-            dataMap: property.toJson(property),
-          );
-
-          return result.fold(
-            (failure) => Left(Failure(message: failure.message)),
-            (SuccessMessage) => Right(SuccessMessage),
-          );
-        },
+        (SuccessMessage) => Right(SuccessMessage),
       );
     } on FirebaseException catch (e) {
       return Left(Failure(message: e.message!));
+    } on Failure catch (e) {
+      if (e.message == 'permission_denied') {
+        return const Left(Failure(
+            message:
+                "User is not a landlord. Cannot create a new property with tenant credentials."));
+      } else {
+        return const Left(Failure(message: 'Something went wrong.'));
+      }
     } catch (e) {
       return Left(Failure(message: e.toString()));
     }
--- test/feature/manage properties/domain/usecases/create_property_usecase_test.dart
+++ test/feature/manage properties/domain/usecases/create_property_usecase_test.dart
@@ -1,25 +1,80 @@
+import 'package:bumblebee/core/exceptions/failure.dart';
 import 'package:bumblebee/core/models/property.dart';
+import 'package:bumblebee/feature/authentication/data/models/user.dart';
 import 'package:bumblebee/feature/manage%20properties/domain/usecases/create_property_usecase.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 
+import 'create_property_usecase_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<CreatePropertyUseCase>()])
 void main() {
   group('Tests about adding a property', () {
     const String propertyName = "Test property";
     const PropertyType type = PropertyType.Single;
     const String address = 'Test address for the new property';
 
+    const String email = 'test@example.com';
+    const String password = '123456';
+    // final createUseCase = MockCreatePropertyUseCase();
+    final FakeFirebaseFirestore firestore = FakeFirebaseFirestore();
+    final MockFirebaseAuth auth = MockFirebaseAuth();
+
     test('CreatePropertyUsecase writes to a property document on firestore.',
         () async {
-      final result = await CreatePropertyUseCase()
-          .createProperty(name: propertyName, type: type, address: address);
+      final result = await CreatePropertyUseCase().createProperty(
+          name: propertyName,
+          type: type,
+          address: address,
+          userRole: userRoles.Tenant,
+          auth: auth,
+          firestore: firestore);
+
       result.fold((failure) => print(failure.message), (successMessage) {
         expect(successMessage,
             'Successfully created new property named $propertyName');
       });
     });
 
+    final mockCreatePropertyUseCase = MockCreatePropertyUseCase();
+
     test(
-        'CreatePropertyUsecase throws failure since user only has tenant privileges.',
-        () {});
+      'CreatePropertyUsecase throws failure when no current user is logged in.',
+      () async {
+        final result = await CreatePropertyUseCase().createProperty(
+            name: propertyName,
+            type: type,
+            address: address,
+            userRole: userRoles.Tenant,
+            auth: auth,
+            firestore: firestore);
+
+        expect(result.fold((l) => l, (r) => r), isA<Failure>());
+        expect(result.fold((l) => l, (r) => r),
+            const Failure(message: 'No current user present.'));
+      },
+    );
+
+    test(
+      'CreatePropertyUsecase throws failure when user only has tenant privileges.',
+      () async {
+        final user = MockUser();
+        final MockFirebaseAuth auth = MockFirebaseAuth(mockUser: user);
+
+        final result = await CreatePropertyUseCase().createProperty(
+            name: propertyName,
+            type: type,
+            address: address,
+            userRole: userRoles.Tenant,
+            auth: auth,
+            firestore: firestore);
+
+        expect(result.fold((l) => l, (r) => r), isA<Failure>());
+        expect(result.fold((l) => l, (r) => r),
+            const Failure(message: 'No current user present.'));
+      },
+    );
   });
 }

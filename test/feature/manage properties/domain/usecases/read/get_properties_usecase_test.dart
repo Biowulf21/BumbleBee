9983f4a9cb93d3e289cc93b1b58e037f3525e271
James Jilhaney <notail2111@gmail.com>
2023-02-24 17:42:50 +0800

    Added test to return failure on getAllPropertiesUsecase when user is unauthenticated

--- test/feature/manage properties/domain/usecases/read/get_properties_usecase_test.dart
+++ test/feature/manage properties/domain/usecases/read/get_properties_usecase_test.dart
@@ -1,3 +1,5 @@
+import 'package:bumblebee/core/exceptions/failure.dart';
+import 'package:bumblebee/feature/manage%20properties/domain/usecases/read/get_properties_usecase.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,7 +9,16 @@ void main() {
   final auth = MockFirebaseAuth(mockUser: user);
   final firestore = FakeFirebaseFirestore();
 
-  group('Testing getPropertiesUseCase by getting all properties', () {
-    
+  group('getPropertiesUseCase returns Failure tests.', () {
+    test(
+        'getPropertiesUseCase returns Failure because user is not authenticated',
+        () async {
+      final result = await GetAllPropertiesUseCase()
+          .getAllPropertiesByID(auth: auth, firestore: firestore);
+
+      expect(result.fold((failure) => failure, (r) => null), isA<Failure>());
+      expect(result.fold((failure) => failure.message, (r) => null),
+          'No user currently logged in. Cannot get properties.');
+    });
   });
 }

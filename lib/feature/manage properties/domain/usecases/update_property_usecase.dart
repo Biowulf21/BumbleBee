b8b7bc0948a0df3324670dba24641426541554fa
James Jilhaney <notail2111@gmail.com>
2023-02-24 14:33:06 +0800

    Created update property method on updateproperty usecase

--- lib/feature/manage properties/domain/usecases/update_property_usecase.dart
+++ lib/feature/manage properties/domain/usecases/update_property_usecase.dart
@@ -1,23 +1,48 @@
 import 'package:bumblebee/core/exceptions/failure.dart';
 import 'package:bumblebee/core/models/property.dart';
+import 'package:bumblebee/core/repositories/firestore_repository.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class IUpdatePropertyUsecase {
-  Future<Either<Failure, String>> updateProperty(
-      {required String propertyID,
-      Map<String, dynamic>? dataMap,
-      Property? propertyObj});
+  Future<Either<Failure, String>> updateProperty({
+    required String propertyID,
+    required FirebaseAuth auth,
+    required FirebaseFirestore firestore,
+    Map<String, dynamic>? dataMap,
+    Property? propertyObj,
+  });
 }
 
 class UpdatePropertyUsecase implements IUpdatePropertyUsecase {
   @override
   Future<Either<Failure, String>> updateProperty(
       {required String propertyID,
+      required FirebaseAuth auth,
+      required FirebaseFirestore firestore,
       Map<String, dynamic>? dataMap,
       Property? propertyObj}) async {
+    if (auth.currentUser == null) {
+      return const Left(Failure(
+          message: 'No user is logged in. Cannot create a new property.'));
+    }
+
     if (dataMap == null && propertyObj == null) {
       return const Left(
           Failure(message: 'Cannot update property. No data provided.'));
     }
+
+    final result = await FirestoreRepository(firestore).updateDocument(
+        collectionID: 'Properties/$propertyID',
+        successMessage: "Successfully updated property.",
+        dataMap: dataMap ?? propertyObj!.toJson(propertyObj),
+        documentName: propertyID);
+
+    return result.fold((failure) {
+      return Left(failure);
+    }, (successMessage) {
+      return Right(successMessage);
+    });
   }
 }
---
+++ test/feature/manage properties/domain/usecases/update/update_property_usecase_test.dart
@@ -0,0 +1,22 @@
+import 'package:bumblebee/feature/manage%20properties/domain/usecases/update_property_usecase.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateNiceMocks([MockSpec<UpdatePropertyUsecase>()])
+void main() {
+  group("Testing UpdatePropertyUsecase", () {
+    final user = MockUser();
+    final auth = MockFirebaseAuth(mockUser: user);
+    final firestore = FakeFirebaseFirestore();
+    test('UpdatePropertyUsecase returns Failure since user is unauthenticated',
+        () async {
+      final result = await UpdatePropertyUsecase().updateProperty(
+          propertyID: 'testid', auth: auth, firestore: firestore);
+
+      expect(result.fold((failure) => null, (successMessage) => successMessage),
+          'No user is logged in. Cannot create a new property.');
+    });
+  });
+}

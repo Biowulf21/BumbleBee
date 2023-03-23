537a5331acd2532dc6b85ead0e2da722909e45c9
James Jilhaney <notail2111@gmail.com>
2023-03-23 18:58:23 +0800

    Added validation of string or int depending on a boolean value (if textfield is enabled or not)

--- lib/core/repositories/input_validator_repository.dart
+++ lib/core/repositories/input_validator_repository.dart
@@ -81,4 +81,27 @@ class InputValidator {
 
     return 'Please enter a valid Philippine phone number.';
   }
+
+  static String? validateIfTextFieldIsEnabled(
+      {String? stringValue, int? intValue, required bool boolCheck}) {
+    if (boolCheck == false) {
+      return "Textfield is disabled but has value passed to InputValidator class.";
+    }
+
+    if (stringValue == null && intValue == null) {
+      return "No value passed to InputValidator class despite textfield being active";
+    }
+
+    // Returns null if either stringValue or intValue have values
+    // Returns errors if both do not have values or both have values
+    if (stringValue == null) {
+      if (intValue != null) return null;
+      return "No values passed to intValue or StringValue of InputValidator class";
+    } else {
+      if (intValue != null) {
+        return "No value passed to InputValidator class despite textfield being active";
+      }
+      return null;
+    }
+  }
 }
